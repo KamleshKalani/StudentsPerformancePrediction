3e8355950634bcfcf30cc342fc15e3c19af77d85
unknown <kamleshkalani45@example.com>
2024-06-08 09:41:01 +0500

    Data Transformation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.StudentsPerformancePrediction.logger import logging
 from src.StudentsPerformancePrediction.exception import CustomException
 from src.StudentsPerformancePrediction.components.data_ingestion import DataIngestion
 from src.StudentsPerformancePrediction.components.data_ingestion import DataIngestionConfig
+from src.StudentsPerformancePrediction.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
 
 
@@ -11,6 +12,9 @@ if __name__=="__main__":
     try:
         data_ingestion=DataIngestion()
         train_data_path,test_data_path= data_ingestion.initiate_data_ingestion()
+        
+        data_transformation =DataTransformation()
+        data_transformation.initiate_data_transformer(train_data_path,test_data_path)
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
